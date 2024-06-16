a445976f3ae7674b5a944de0bcdc12223a31ceb3
Subhanshu <subhanshusingh3680@gmail.com>
2024-06-16 14:52:05 +0530

    setup backend

---
+++ backend/config/database.js
@@ -0,0 +1,12 @@
+const mongoose = require('mongoose');
+const databaseConnect = () => {
+    mongoose.connect(process.env.DB_URI)
+        .then((data) => {
+            console.log(`Mongodb connected with server: ${data.connection.host}`);
+        })
+        .catch((err) => {
+            console.logr(err);
+        })
+}
+
+module.exports = databaseConnect;
--- backend/server.js
+++ backend/server.js
@@ -1,9 +1,14 @@
 const app = require('./app');
 const dotenv = require('dotenv');
 
+//importing mongodb connecting function
+const databaseConnect = require('./config/database');
+
 //config
 dotenv.config({ path: 'backend/config/config.env' });
 
+//connecting to database
+databaseConnect();
 
 app.listen(process.env.PORT, () => {
     console.log(`Server is working on http://localhost:${process.env.PORT}`);
