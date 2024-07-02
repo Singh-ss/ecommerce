3e7aabe0b3ef2811ce3d9a2aec96016b7809bb0e
Subhanshu <subhanshusingh3680@gmail.com>
2024-07-02 17:10:35 +0530

    Add deployment changes

--- backend/app.js
+++ backend/app.js
@@ -6,6 +6,7 @@ const cookieParser = require('cookie-parser');
 
 const bodyParser = require('body-parser');
 const fileUpload = require('express-fileupload');
+const path = require("path");
 
 // Config
 if (process.env.NODE_ENV !== "PRODUCTION") {
@@ -27,6 +28,12 @@ app.use('/api/v1', product);
 app.use('/api/v1', user);
 app.use('/api/v1', order);
 app.use("/api/v1", payment);
+
+app.use(express.static(path.join(__dirname, "../frontend/build")));
+
+app.get('*', (req, res) => {
+    res.sendFile(path.resolve(__dirname, "../frontend/build/index.html"))
+});
 
 //middleware for errors
 app.use(errorMiddleware);
